c3d270f0f97a45fffcae732084b4572b9b817459
William Leung <leungwk25@gmail.com>
2018-02-25 01:18:57 -0800

    Setup boilerplate

---
+++ app/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Container from "./components/Container";
+import TextFormEntry from "./components/TextFormEntry";
+
+// Creating an App component which renders a ListContainer inside of a Container
+const App = () => (
+  <Container>
+    <TextFormEntry/>
+  </Container>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
